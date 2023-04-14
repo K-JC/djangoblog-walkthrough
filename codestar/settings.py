e7c434d6b0bdbb2f6033e496ddf43aeb2cab92d6
K-JC <kellytolfts93@hotmail.com>
2023-04-14 12:28:43 +0000

    deployment commit

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTION = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['codestar-blog-project-1.herokuapp.com', 'localhost']
 
